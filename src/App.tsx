4732f84a813e8ef10ad08f9c7037298341dc5c88
Eligiani-Monteiro <eligianedasilva32@gmail.com>
2022-11-07 19:41:39 -0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { FormUser } from "./components/FormUser";
+import { GlobalStyle } from "./globalStyle";
+import  'bootstrap/dist/css/bootstrap.min.css' ;
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle/>
+      <FormUser/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/FormUser/index.tsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import { Col, Form, Row, Button} from "react-bootstrap";
+import { getCep } from "../../api/cep";
+import { FormUserStyle } from './styled';
+import { BeatLoader } from "react-spinners";
+
+export function FormUser() {
+
+    const [cep, setCep] = useState('01001000')
+    const [isLoading, setIsLoading] = useState(false)
+    const [endereco, setEndereco] = useState('')
+    const [localidade, setLocalidade] = useState('')
+    const [uf, setUf] = useState('')
+
+    const listAdress = async () => {
+        const address = await getCep(cep)
+        setEndereco(address.logradouro)
+        setLocalidade(address.localidade)
+        setUf(address.uf)
+        setIsLoading(false)
+    }
+
+    useEffect(() => {
+        listAdress()
+    }, [])
+    let text = ''
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+
+        if (event.key === "Enter") {
+            setIsLoading(true)
+            listAdress()
+        } else {
+            text = event.target.value;
+            setCep(text)
+        }
+    }
+
+
+    return (
+        <FormUserStyle>
+            <Form>
+                <Row>
+                    <h3>Cadastro Clientes</h3>
+                </Row>
+                <Form.Group className="mb-12" controlId="formBasicName">
+                    <Form.Label>Nome: </Form.Label>
+                    <Form.Control type="text" placeholder="Digite seu Nome" />
+                </Form.Group>
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="formBasicRG">
+                            <Form.Label column>RG: </Form.Label>
+                            <Form.Control type="text" placeholder="RG" />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="formBasicCPF">
+                            <Form.Label column>CPF: </Form.Label>
+                            <Form.Control type="text" placeholder="CPF" />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="formBasicDTNascimento">
+                            <Form.Label column>Data de Nascimento: </Form.Label>
+                            <Form.Control type="date" placeholder="" />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    {isLoading ? <BeatLoader color="#c536d6" /> : ''}
+                    <Col>
+                        <Form.Group className="mb-3" controlId="formBasicCEP">
+                            <Form.Label column>CEP: </Form.Label>
+                            <Form.Control type="text" placeholder="CEP" onChange={handleChange} />
+                        </Form.Group>
+                    </Col>
+                    <Col xs={8}>
+                        <Form.Group className="mb-3" controlId="formBasicEndereco">
+                            <Form.Label column>Endereço: </Form.Label>
+                            <Form.Control type="text" placeholder="Endereço" value={endereco} onChange={() => { }} />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="formBasicCidade">
+                            <Form.Label>Cidade: </Form.Label>
+                            <Form.Control type="text" placeholder="Cidade" value={localidade} onChange={() => { }} />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="formBasicEstado">
+                            <Form.Label>Estado: </Form.Label>
+                            <Form.Control type="text" placeholder="Estado" value={uf} onChange={() => { }} />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Button variant="primary" type="submit">
+                    Salvar
+                </Button>
+            </Form>
+        </FormUserStyle>
+    )
+}
